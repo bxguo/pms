6d5ca3118668ff97408b0a6f1aaca52dfeabb450
bxguo <951409002@qq.com>
2019-03-19 18:02:44 +0800

    fix:目录调整

---
+++ pms-rule/src/main/java/com/zysl/cloud/pms/rule/OpenpmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.zysl.cloud.pms.rule;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenpmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenpmsApiApplication.class, args);
+    }
+
+}
---
+++ pms-web/src/main/java/com/zysl/cloud/pms/web/WebApplication.java
@@ -0,0 +1,16 @@
+package com.zysl.cloud.pms.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan({"com.zysl.cloud"})
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
